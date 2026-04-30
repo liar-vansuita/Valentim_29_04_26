631b4ecb3967aa57571e9a535ff338f589f711f0
Lia <julia_vansuita@estudante.sesisenai.org.br>
2026-04-29 21:14:05 -0300

    Também

---
+++ LojaDB3.sql
@@ -0,0 +1,9 @@
+-- Consultas SQL 
+
+	-- Query -- 
+/*
+	Gossip Version:
+		Query é só um pedido de informação — uma perguntinha direta pra um sistema que guarda dados.
+	Gissip Direct:
+		Uma query é um comando usado para buscar, filtrar, inserir ou modificar dados em um banco de dados.
+*/
